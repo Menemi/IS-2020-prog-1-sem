6e6dfa21fd4961cbaf5b6b8ef208e6b5f54fefc4
DAH9 <danila.titov1210@yandex.ru>
2021-03-26 02:28:26 +0300

    Initial commit

---
+++ block1/1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float temp;
+    printf("Input float number: ");
+    scanf_s("%f", &temp);
+    printf("You typed this number: %f\n\n", temp);
+
+    char temp2;
+    printf("Input symbol: ");
+    scanf_s("%c", &temp2);
+    printf("You typed this symbol: %c\n", temp2);
+}
